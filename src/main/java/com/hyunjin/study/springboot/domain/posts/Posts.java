a143f1a038c99b779fe0c92f5c5758dbd1e0c509
kim-hyunjin <hyunjin1612@gmail.com>
2020-09-01 19:27:12 +0900

    등록/수정/조회 API만들기

--- src/main/java/com/hyunjin/study/springboot/domain/posts/Posts.java
+++ src/main/java/com/hyunjin/study/springboot/domain/posts/Posts.java
@@ -30,6 +30,11 @@ public class Posts {
         this.author = author;
     }
 
+    public void update(String title, String content) { // JPA 영속성 컨텍스트. 트랜잭션 안에서 데이터베이스에서 데이터를 가져오면 영속성 컨텍스트가 유지된 상태. 이 상태에서 데이터가 변경되면 트랜잭션이 끝나는 시점에 해당 테이블에 데이터 변경분을 반영한다.
+        this.title = title;
+        this.content = content;
+    }
+
     // Setter를 만들지 않는 이유는 인스턴스 값들이 언제 어디서 변해야 하는지 코드상으로 명확하게 구분할 수 없어 차후 기능 변경시 복잡해지기 때문이다.
     // Setter가 없기 때문에 DB에 값을 삽입하기 위해서 기본적으로 생성자를 사용한다.
     // 값 변경이 필요한 경우 해당 이벤트에 맞는 public 메소드를 호출하여 변경한다.
