96dce37aa2e6d5e7653d31f8fe8ddbf935b9614a
kim-hyunjin <hyunjin1612@gmail.com>
2020-09-08 20:41:13 +0900

    게시글 목록 최종수정날짜 표기 변경

--- src/main/java/com/hyunjin/study/springboot/web/dto/PostsListResponseDto.java
+++ src/main/java/com/hyunjin/study/springboot/web/dto/PostsListResponseDto.java
@@ -4,6 +4,7 @@ import com.hyunjin.study.springboot.domain.posts.Posts;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 public class PostsListResponseDto {
@@ -11,11 +12,13 @@ public class PostsListResponseDto {
     private String title;
     private String author;
     private LocalDateTime modifiedDate;
+    private String formattedDate;
 
     public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
         this.modifiedDate = entity.getModifiedDate();
+        this.formattedDate = modifiedDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 }
